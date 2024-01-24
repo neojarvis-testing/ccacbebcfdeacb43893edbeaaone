05eececa722d500dfe14687b31dbdd61ee72162c
neojarvis <techadmin@iamneo.ai>
2024-01-24 08:06:11 +0000

    Submit-project-save-commit-24/01/2024--13:36:03

---
+++ pytest/run.sh
@@ -0,0 +1,3 @@
+cp /home/coder/project/workspace/pytest/tests.py /home/coder/project/workspace/Greet_with_time_project/greetapp/tests.py
+cd /home/coder/project/workspace/Greet_with_time_project
+python3 manage.py test -v 0
